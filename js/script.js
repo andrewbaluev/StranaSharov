80c3f41487489a5fd1bfc4841cc5b28cf27f8317
Andrew Baluev <damico750@yandex.ru>
2021-01-12 23:18:33 +0300

    add scss files, change sidebar

--- js/script.js
+++ js/script.js
@@ -14,5 +14,56 @@ $(document).ready(function(){
     $('#nav-icon').click(function(event){
         event.preventDefault();
         $(this).toggleClass('open');
-    });    
+    });
+
+    // Fixed Header
+
+    const header = document.querySelector('.header');
+
+    window.onscroll = () => {
+        if (window.pageYOffset > 100) {
+            header.classList.add('header__active');
+        } else {
+            header.classList.remove('header__active');
+        }
+    };
+
+    // Slider
+
+    const slides = document.querySelectorAll('.intro__slider-item');
+    const activeClass = 'intro__slider-item-visible';
+    let index = 0;
+
+    setInterval(function() {
+        slides[index].classList.remove(activeClass);
+        index++;
+        if (index + 1 > slides.length) {
+            index = 0;
+        }
+        slides[index].classList.add(activeClass);
+    }, 3000);
+
+    // Sidebar 
+
+    const menuOpen = document.querySelector('.header__nav-open');
+    const menuClose = document.querySelector('.header__nav-close');
+    const sidebar = document.querySelector('.header__nav');
+    const menuLinks = document.querySelectorAll('.header__link');
+
+    menuOpen.addEventListener('click', () => {
+        sidebar.classList.add('header__nav-active');
+    });
+
+    menuClose.addEventListener('click', () => {
+        sidebar.classList.remove('header__nav-active');
+    });
+
+    if (window.innerWidth <= 767) {
+        for (let i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+                sidebar.classList.remove('header__nav-active');
+            });
+        }
+    }
+
 });
